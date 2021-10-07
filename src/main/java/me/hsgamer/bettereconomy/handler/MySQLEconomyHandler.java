40b885e3f5b67abd1bebc0f229fad5feaa9ec6b8
Kazoku <takahatashunvn@gmail.com>
2021-10-07 21:33:58 +0700

    fixed SQL bugs (#3)
    * added MySQL support
    
    * no more maven shade warning
    
    * fixed SQL bugs

--- src/main/java/me/hsgamer/bettereconomy/handler/MySQLEconomyHandler.java
+++ src/main/java/me/hsgamer/bettereconomy/handler/MySQLEconomyHandler.java
@@ -7,13 +7,15 @@ import me.hsgamer.hscore.database.client.sql.java.JavaSqlClient;
 import me.hsgamer.hscore.database.driver.MySqlDriver;
 import org.bukkit.OfflinePlayer;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 
 public class MySQLEconomyHandler extends EconomyHandler {
 
-  private JavaSqlClient client;
+  private Connection connection;
 
   public MySQLEconomyHandler(BetterEconomy instance) {
     super(instance);
@@ -24,27 +26,26 @@ public class MySQLEconomyHandler extends EconomyHandler {
     setting.setUsername(instance.getMainConfig().getMysqlUsername());
     setting.setPassword(instance.getMainConfig().getMysqlPassword());
     try {
-      client = new JavaSqlClient(setting, new MySqlDriver());
-    } catch (ClassNotFoundException e) {
-      instance.getLogger().log(Level.SEVERE, "constructor()", e);
+      JavaSqlClient client = new JavaSqlClient(setting, new MySqlDriver());
+      connection = client.getConnection();
+    } catch (ClassNotFoundException | SQLException e) {
+      instance.getLogger().log(Level.SEVERE, "constructor()#connection", e);
       return;
     }
-    final String sql = "CREATE TABLE IF NOT EXISTS ?.`economy` (`uuid` varchar(36) NOT NULL UNIQUE, `balance` double DEFAULT 0);";
-    try (PreparedStatement stmt = client.getConnection().prepareStatement(sql)) {
-      stmt.setString(1, setting.getDatabaseName());
+    final String sql = "CREATE TABLE IF NOT EXISTS `economy` (`uuid` varchar(36) NOT NULL UNIQUE, `balance` double DEFAULT 0);";
+    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
       stmt.execute();
     } catch (SQLException e) {
-      instance.getLogger().log(Level.SEVERE, "constructor()", e);
+      instance.getLogger().log(Level.SEVERE, "constructor()#createTable", e);
     }
   }
 
   @Override
   public boolean hasAccount(OfflinePlayer player) {
-    final String sql = "SELECT * FROM ?.`economy` WHERE `uuid` = ?";
-    try (PreparedStatement stmt = client.getConnection().prepareStatement(sql)) {
-      stmt.setString(1, client.getSetting().getDatabaseName());
-      stmt.setString(2, player.getUniqueId().toString());
-      return stmt.execute();
+    final String sql = "SELECT * FROM `economy` WHERE `uuid` = ?";
+    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+      stmt.setString(1, player.getUniqueId().toString());
+      return stmt.executeQuery().next();
     } catch (SQLException e) {
       instance.getLogger().log(Level.SEVERE, "hasAccount()", e);
       return false;
@@ -53,11 +54,11 @@ public class MySQLEconomyHandler extends EconomyHandler {
 
   @Override
   public double get(OfflinePlayer player) {
-    final String sql = "SELECT `balance` FROM ?.`economy` WHERE `uuid` = ?";
-    try (PreparedStatement stmt = client.getConnection().prepareStatement(sql)) {
-      stmt.setString(1, client.getSetting().getDatabaseName());
-      stmt.setString(2, player.getUniqueId().toString());
-      return stmt.executeQuery().getDouble("balance");
+    final String sql = "SELECT `balance` FROM `economy` WHERE `uuid` = ?";
+    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+      stmt.setString(1, player.getUniqueId().toString());
+      ResultSet result = stmt.executeQuery();
+      return result.next() ? result.getDouble("balance") : 0.0;
     } catch (SQLException e) {
       instance.getLogger().log(Level.SEVERE, "get()", e);
       return 0;
@@ -74,12 +75,11 @@ public class MySQLEconomyHandler extends EconomyHandler {
     if (amount < 0) {
       return false;
     }
-    final String sql = "UPDATE ?.`economy` SET `balance` = ? WHERE `uuid` = ?";
-    try (PreparedStatement stmt = client.getConnection().prepareStatement(sql)) {
-      stmt.setString(1, client.getSetting().getDatabaseName());
-      stmt.setDouble(2, amount);
-      stmt.setString(3, player.getUniqueId().toString());
-      stmt.execute(sql);
+    final String sql = "UPDATE `economy` SET `balance`= ? WHERE `uuid`= ?";
+    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+      stmt.setDouble(1, amount);
+      stmt.setString(2, player.getUniqueId().toString());
+      stmt.executeUpdate();
     } catch (SQLException e) {
       instance.getLogger().log(Level.SEVERE, "set()", e);
     }
@@ -91,12 +91,11 @@ public class MySQLEconomyHandler extends EconomyHandler {
     if (hasAccount(player)) {
       return false;
     }
-    final String sql = "INSERT INTO ?.`economy`  (`uuid`, `balance`) VALUES (?, ?) ";
-    try (PreparedStatement stmt = client.getConnection().prepareStatement(sql)) {
-      stmt.setString(1, client.getSetting().getDatabaseName());
-      stmt.setString(2, player.getUniqueId().toString());
-      stmt.setDouble(3, instance.getMainConfig().getStartAmount());
-      stmt.execute(sql);
+    final String sql = "INSERT INTO `economy`  (`uuid`, `balance`) VALUES ( ? , ? ) ";
+    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+      stmt.setString(1, player.getUniqueId().toString());
+      stmt.setDouble(2, instance.getMainConfig().getStartAmount());
+      stmt.executeUpdate();
     } catch (SQLException e) {
       instance.getLogger().log(Level.SEVERE, "createAccount()", e);
     }
@@ -106,7 +105,7 @@ public class MySQLEconomyHandler extends EconomyHandler {
   @Override
   public void disable() {
     try {
-      client.getConnection().close();
+      connection.close();
     } catch (SQLException e) {
       instance.getLogger().log(Level.SEVERE, "disable()", e);
     }
