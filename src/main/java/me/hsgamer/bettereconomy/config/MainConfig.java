f0ff4b115b501c6a61032b309b73b7208614ed1b
Huynh Tien <huynhqtienvtag@gmail.com>
2021-10-07 20:40:17 +0700

    reformat

--- src/main/java/me/hsgamer/bettereconomy/config/MainConfig.java
+++ src/main/java/me/hsgamer/bettereconomy/config/MainConfig.java
@@ -9,19 +9,31 @@ import org.bukkit.plugin.Plugin;
 @Getter
 @SuppressWarnings("all")
 public class MainConfig extends AnnotatedConfig {
-    private @ConfigPath("handler-type") String handlerType = "file";
-    private @ConfigPath("currency.singular") String currencySingular = "$";
-    private @ConfigPath("currency.plural") String currencyPlural = "$";
-    private @ConfigPath("currency.format-fractional-digits") int fractionalDigits = 2;
-    private @ConfigPath("balance.top-update-period") long updateBalanceTopPeriod = 100;
-    private @ConfigPath("balance.file-save-period") long saveFilePeriod = 200;
-    private @ConfigPath("balance.start-amount") double startAmount = 0;
+    private @ConfigPath("handler-type")
+    String handlerType = "file";
+    private @ConfigPath("currency.singular")
+    String currencySingular = "$";
+    private @ConfigPath("currency.plural")
+    String currencyPlural = "$";
+    private @ConfigPath("currency.format-fractional-digits")
+    int fractionalDigits = 2;
+    private @ConfigPath("balance.top-update-period")
+    long updateBalanceTopPeriod = 100;
+    private @ConfigPath("balance.file-save-period")
+    long saveFilePeriod = 200;
+    private @ConfigPath("balance.start-amount")
+    double startAmount = 0;
 
-    private @ConfigPath("database.mysql.host") String mysqlHost = "localhost";
-    private @ConfigPath("database.mysql.port") String mysqlPort = "3306";
-    private @ConfigPath("database.mysql.dbname") String mysqlDatabaseName = "";
-    private @ConfigPath("database.mysql.username") String mysqlUsername = "root";
-    private @ConfigPath("database.mysql.password") String mysqlPassword = "";
+    private @ConfigPath("database.mysql.host")
+    String mysqlHost = "localhost";
+    private @ConfigPath("database.mysql.port")
+    String mysqlPort = "3306";
+    private @ConfigPath("database.mysql.dbname")
+    String mysqlDatabaseName = "";
+    private @ConfigPath("database.mysql.username")
+    String mysqlUsername = "root";
+    private @ConfigPath("database.mysql.password")
+    String mysqlPassword = "";
 
     public MainConfig(Plugin plugin) {
         super(new BukkitConfig(plugin, "config.yml"));
--- src/main/java/me/hsgamer/bettereconomy/config/MessageConfig.java
+++ src/main/java/me/hsgamer/bettereconomy/config/MessageConfig.java
@@ -9,19 +9,32 @@ import org.bukkit.plugin.Plugin;
 @Getter
 @SuppressWarnings("all")
 public class MessageConfig extends AnnotatedConfig {
-    private @ConfigPath("prefix") String prefix = "&f[&6BetterEconomy&f] &r";
-    private @ConfigPath("player-not-found") String playerNotFound = "&cThe player is not found";
-    private @ConfigPath("player-only") String playerOnly = "&cThis command is for player only";
-    private @ConfigPath("balance-output") String balanceOutput = "&eBalance: &f{balance}";
-    private @ConfigPath("balance-top-output") String balanceTopOutput = "&f#{place} &e{name}: &f{balance}";
-    private @ConfigPath("empty-balance-top") String emptyBalanceTop = "&eThe balance top is empty";
-    private @ConfigPath("arg-not-found") String argNotFound = "&cInvalid Argument";
-    private @ConfigPath("invalid-amount") String invalidAmount = "&cInvalid Amount";
-    private @ConfigPath("give-success") String giveSuccess = "&aSuccessfully give {balance} to {name}";
-    private @ConfigPath("take-success") String takeSuccess = "&aSuccessfully take {balance} from {name}";
-    private @ConfigPath("set-success") String setSuccess = "&aSuccessfully set {balance} to {name}";
-    private @ConfigPath("success") String success = "&aSuccess";
-    private @ConfigPath("cannot-do") String cannotDo = "&cCannot do this action";
+    private @ConfigPath("prefix")
+    String prefix = "&f[&6BetterEconomy&f] &r";
+    private @ConfigPath("player-not-found")
+    String playerNotFound = "&cThe player is not found";
+    private @ConfigPath("player-only")
+    String playerOnly = "&cThis command is for player only";
+    private @ConfigPath("balance-output")
+    String balanceOutput = "&eBalance: &f{balance}";
+    private @ConfigPath("balance-top-output")
+    String balanceTopOutput = "&f#{place} &e{name}: &f{balance}";
+    private @ConfigPath("empty-balance-top")
+    String emptyBalanceTop = "&eThe balance top is empty";
+    private @ConfigPath("arg-not-found")
+    String argNotFound = "&cInvalid Argument";
+    private @ConfigPath("invalid-amount")
+    String invalidAmount = "&cInvalid Amount";
+    private @ConfigPath("give-success")
+    String giveSuccess = "&aSuccessfully give {balance} to {name}";
+    private @ConfigPath("take-success")
+    String takeSuccess = "&aSuccessfully take {balance} from {name}";
+    private @ConfigPath("set-success")
+    String setSuccess = "&aSuccessfully set {balance} to {name}";
+    private @ConfigPath("success")
+    String success = "&aSuccess";
+    private @ConfigPath("cannot-do")
+    String cannotDo = "&cCannot do this action";
 
     public MessageConfig(Plugin plugin) {
         super(new BukkitConfig(plugin, "messages.yml"));
