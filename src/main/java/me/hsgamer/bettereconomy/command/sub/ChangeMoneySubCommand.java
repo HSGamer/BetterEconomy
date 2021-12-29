3737e9e1e497aef3056faa9f93998bbbeeb247f5
Huynh Tien <huynhqtienvtag@gmail.com>
2021-12-29 18:13:29 +0700

    fix 0 amount

--- src/main/java/me/hsgamer/bettereconomy/command/sub/ChangeMoneySubCommand.java
+++ src/main/java/me/hsgamer/bettereconomy/command/sub/ChangeMoneySubCommand.java
@@ -35,7 +35,7 @@ public abstract class ChangeMoneySubCommand extends SubCommand {
             MessageUtils.sendMessage(sender, instance.getMessageConfig().getPlayerNotFound());
             return;
         }
-        Optional<Double> amount = Validate.getNumber(args[1]).map(BigDecimal::doubleValue).filter(value -> value > 0);
+        Optional<Double> amount = Validate.getNumber(args[1]).map(BigDecimal::doubleValue).filter(value -> value >= 0);
         if (!amount.isPresent()) {
             MessageUtils.sendMessage(sender, instance.getMessageConfig().getInvalidAmount());
             return;
