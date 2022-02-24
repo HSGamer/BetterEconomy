f784915b43788ae5c2b9019ad0e277e7426d3b3a
HSGamer <huynhqtienvtag@gmail.com>
2022-02-24 09:32:31 +0700

    create account on pre login

--- src/main/java/me/hsgamer/bettereconomy/listener/JoinListener.java
+++ src/main/java/me/hsgamer/bettereconomy/listener/JoinListener.java
@@ -2,8 +2,9 @@ package me.hsgamer.bettereconomy.listener;
 
 import me.hsgamer.bettereconomy.BetterEconomy;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 
 import java.util.UUID;
 
@@ -14,9 +15,10 @@ public class JoinListener implements Listener {
         this.instance = instance;
     }
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        UUID uuid = event.getPlayer().getUniqueId();
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPreLogin(AsyncPlayerPreLoginEvent event) {
+        if (event.getLoginResult() != AsyncPlayerPreLoginEvent.Result.ALLOWED) return;
+        UUID uuid = event.getUniqueId();
         if (!instance.getEconomyHandler().hasAccount(uuid)) {
             instance.getEconomyHandler().createAccount(uuid);
         }
