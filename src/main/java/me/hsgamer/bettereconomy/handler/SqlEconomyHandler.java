5b9d58a49a3a6ab042b9f57cd9e250092350f7f4
HSGamer <huynhqtienvtag@gmail.com>
2022-02-28 23:19:29 +0700

    use new StatementBuilder in SqlEconomyHandler

--- src/main/java/me/hsgamer/bettereconomy/handler/SqlEconomyHandler.java
+++ src/main/java/me/hsgamer/bettereconomy/handler/SqlEconomyHandler.java
@@ -4,13 +4,12 @@ import me.hsgamer.bettereconomy.BetterEconomy;
 import me.hsgamer.bettereconomy.api.EconomyHandler;
 import me.hsgamer.hscore.database.Driver;
 import me.hsgamer.hscore.database.Setting;
-import me.hsgamer.hscore.database.client.sql.PreparedStatementContainer;
+import me.hsgamer.hscore.database.client.sql.StatementBuilder;
 import me.hsgamer.hscore.database.client.sql.java.JavaSqlClient;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.UUID;
 import java.util.logging.Level;
 
@@ -23,63 +22,41 @@ public abstract class SqlEconomyHandler extends EconomyHandler {
         try {
             JavaSqlClient client = new JavaSqlClient(setting, driver);
             connection = client.getConnection();
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             throw new IllegalStateException("constructor()#connection", e);
         }
-        try (Statement statement = connection.createStatement()) {
-            statement.execute("CREATE TABLE IF NOT EXISTS `economy` (`uuid` varchar(36) NOT NULL UNIQUE, `balance` double DEFAULT 0);");
-        } catch (SQLException e) {
-            throw new IllegalStateException("constructor()#createTable", e);
-        }
+        StatementBuilder.create(connection)
+                .setStatement("CREATE TABLE IF NOT EXISTS `economy` (`uuid` varchar(36) NOT NULL UNIQUE, `balance` double DEFAULT 0);")
+                .updateSafe();
     }
 
     @Override
     public boolean hasAccount(UUID uuid) {
-        try (
-                PreparedStatementContainer container = PreparedStatementContainer.of(
-                        connection, "SELECT * FROM `economy` WHERE `uuid` = ?",
-                        uuid.toString()
-                );
-                ResultSet resultSet = container.query()
-        ) {
-            return resultSet.next();
-        } catch (SQLException e) {
-            instance.getLogger().log(Level.SEVERE, "hasAccount()", e);
-            return false;
-        }
+        return StatementBuilder.create(connection)
+                .setStatement("SELECT * FROM `economy` WHERE `uuid` = ?")
+                .addValues(uuid.toString())
+                .querySafe(ResultSet::next)
+                .orElse(false);
     }
 
     @Override
     public double get(UUID uuid) {
-        try (
-                PreparedStatementContainer container = PreparedStatementContainer.of(
-                        connection, "SELECT `balance` FROM `economy` WHERE `uuid` = ?",
-                        uuid.toString()
-                );
-                ResultSet resultSet = container.query()
-        ) {
-            return resultSet.next() ? resultSet.getDouble("balance") : 0.0;
-        } catch (SQLException e) {
-            instance.getLogger().log(Level.SEVERE, "get()", e);
-            return 0;
-        }
+        return StatementBuilder.create(connection)
+                .setStatement("SELECT `balance` FROM `economy` WHERE `uuid` = ?")
+                .addValues(uuid.toString())
+                .querySafe(resultSet -> resultSet.next() ? resultSet.getDouble("balance") : 0.0)
+                .orElse(0D);
     }
 
     @Override
     public boolean set(UUID uuid, double amount) {
         if (amount < 0) {
             return false;
-        }
-        try (
-                PreparedStatementContainer container = PreparedStatementContainer.of(
-                        connection, "UPDATE `economy` SET `balance`= ? WHERE `uuid`= ?",
-                        amount, uuid.toString()
-                )
-        ) {
-            return container.update() > 0;
-        } catch (SQLException e) {
-            instance.getLogger().log(Level.SEVERE, "set()", e);
-            return false;
+        } else {
+            return StatementBuilder.create(connection)
+                    .setStatement("UPDATE `economy` SET `balance`= ? WHERE `uuid`= ?")
+                    .addValues(amount, uuid.toString())
+                    .updateSafe() > 0;
         }
     }
 
@@ -87,17 +64,11 @@ public abstract class SqlEconomyHandler extends EconomyHandler {
     public boolean createAccount(UUID uuid, double startAmount) {
         if (hasAccount(uuid)) {
             return false;
-        }
-        try (
-                PreparedStatementContainer container = PreparedStatementContainer.of(
-                        connection, "INSERT INTO `economy` (`uuid`, `balance`) VALUES ( ? , ? )",
-                        uuid.toString(), startAmount
-                )
-        ) {
-            return container.update() > 0;
-        } catch (SQLException e) {
-            instance.getLogger().log(Level.SEVERE, "createAccount()", e);
-            return false;
+        } else {
+            return StatementBuilder.create(connection)
+                    .setStatement("INSERT INTO `economy` (`uuid`, `balance`) VALUES ( ? , ? )")
+                    .addValues(uuid.toString(), startAmount)
+                    .updateSafe() > 0;
         }
     }
 
